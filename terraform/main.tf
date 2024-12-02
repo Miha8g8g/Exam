510fb7daafdee74ff62a634f6fcc345150b1c830
Miha8g8g <144118262+Miha8g8g@users.noreply.github.com>
2024-12-02 16:10:33 +0200

    £5

---
+++ terraform/main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+backend "s3" {
+    bucket = "lab6-my-tf"
+    key = "terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "lab6-tf-lockid" 
+
+  }
+}
+
+
+# Configure the AWS provider
+provider "aws" {
+  region     = "eu-central-1"
+}
+
+resource "aws_security_group" "web_app" {
+  name        = "web_app"
+  description = "security group"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "web_app"
+  }
+}
+
+resource "aws_instance" "webapp_instance" {
+  ami           = "ami-003059224799519de"
+  instance_type = "t2.micro"
+  security_groups= ["web_app"]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo groupadd docker
+  sudo usermod -aG docker ubuntu
+  newgrp docker
+  docker pull miha8g8g/aws:latest
+  docker run -id miha8g8g/aws:latest
+  EOF
+  tags = {
+    Name = "webapp_instance"
+  }
+}
+
+output "instance_public_ip" {
+  value     = aws_instance.webapp_instance.public_ip
+  sensitive = true
+}
